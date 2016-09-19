42cf8b049a64e1a2716b652b6420333793597327
ishita21saxena <is16c@my.fsu.edu>
2016-09-19 17:46:42 -0400

    Updated the main function
    Added print output entries for the three new classes Arrow,Axe and MagicStaff being created.
    and updated the main function in the Rolegame class

---
+++ src/edu/fsu/cs/cen5035/RoleGame.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.fsu.cs.cen5035;
+
+/**
+ *
+ * @author Javier
+ */
+public class RoleGame {
+
+    public static void main(String[] args) {
+        Weapon sword = WeaponFactory.getWeapon("sword");
+        System.out.println("Sword has " + sword.hit() + " of damage.");
+        int armor = 20;
+        System.out.println("Swords was able to do " + sword.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+        
+        Weapon arrow = WeaponFactory.getWeapon("arrow");
+        System.out.println("Arrow has " + arrow.hit() + " of damage.");
+        System.out.println("Arrow was able to do " + arrow.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+        
+        Weapon axe = WeaponFactory.getWeapon("axe");
+        System.out.println("Axe has " + axe.hit() + " of damage.");
+        System.out.println("Axe was able to do " + axe.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+        
+        Weapon magicstaff = WeaponFactory.getWeapon("sword");
+        System.out.println("MagicStaff has " + magicstaff.hit() + " of damage.");
+        System.out.println("MagicStaff was able to do " + magicstaff.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+        
+
+
+    }
+}

c388f96eae9279e9051b864a5f635e57f3e5e6f7
ishita21saxena <is16c@my.fsu.edu>
2016-09-19 17:50:15 -0400

    Updated WeaponFactory class
    The entries for new classes being created are updated in the WeaponFactory class for proper implementation of the game

---
+++ src/edu/fsu/cs/cen5035/WeaponFactory.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.fsu.cs.cen5035;
+
+/**
+ *
+ * @author Javier
+ */
+public class WeaponFactory {
+
+    public static Weapon getWeapon(String type) {
+        switch (type) {
+            case "sword":
+                return new Sword();
+            case "arrow":
+                return new Arrow();
+            case "axe":
+                return new Axe();
+            case "magicstaff":
+                return new MagicStaff();
+            default:
+                throw new IllegalArgumentException("Invalid type");
+        }
+    }
+
+}
